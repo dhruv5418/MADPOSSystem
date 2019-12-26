e952b43f5e1793ab055ae92743e35ecf142779bd
Dhruv <dhruvj5418@gmail.com>
2019-12-25 20:22:19 -0500

    Final Modification

--- app/src/main/java/com/example/madpossystem/view/SecondActivity.java
+++ app/src/main/java/com/example/madpossystem/view/SecondActivity.java
@@ -24,6 +24,7 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         mMessageEditText=findViewById(R.id.editText_second);
+
     }
 
     /**
